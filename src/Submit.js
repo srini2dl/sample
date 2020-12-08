ecc386bfc73170013205af31b99d1e4d0bc30a9a
Srinivasan Raman <srini2dl@gmail.com>
2020-12-08 21:19:39 +0200

    1

---
+++ src/Submit.js
@@ -0,0 +1,8 @@
+import React from 'react'
+class Submit extends React.Component {
+    render() {
+        return <div> Nice </div>
+    } 
+}
+
+export default Submit
---
+++ src/user.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+class Users extends React.Component {
+    render() {
+      return <h1>Users 123</h1>
+    }
+  }
+  export default Users
